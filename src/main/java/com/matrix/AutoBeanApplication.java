7ac7c14b973f78961a12ad4ee0a5cfd87d680ab5
admin <794867067@qq.com>
2020-12-06 23:09:01 +0800

    2

--- src/main/java/com/matrix/AutoBeanApplication.java
+++ src/main/java/com/matrix/AutoBeanApplication.java
@@ -4,40 +4,53 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+
+import com.matrix.config.BeanConfig;
 import com.matrix.entity.Cat;
 import com.matrix.entity.Dog;
 import com.matrix.entity.People;
+import com.matrix.entity.Wife;
 
 @Configuration
 public class AutoBeanApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = new AnnotationConfigApplicationContext(AutoBeanApplication.class);
-		People master = context.getBean("people", People.class);
-		master.getCat().shout();
-		master.getDog().shout();
-		System.out.println(master.getName()); 
+		Wife beauty = context.getBean("beauty" , Wife.class);
+		beauty.warning();
 	}
 	
-	@Bean
-	public Cat initCat() {
-		return new Cat();
+	@Configuration
+	static class WifeConfig {
+		@Bean(name="beauty")
+		Wife dataSource() {
+			return new Wife("你快蹩说话了！！");
+		}
 	}
-	@Bean
-	public Dog initDog() {
-		return new Dog();
-	}
+}
+
+
+
+
+
+/**
+	People master = context.getBean("initPeople", People.class);
+	master.getCat().shout();
+	master.getDog().shout();
+	System.out.println(master.getName());  	
 	
-	@Bean(name = "peopleDefalut")
-	public People initPeople() {
-		return new People();
-	}
-	
-	@Bean(name = "people")		// https://www.cnblogs.com/maohuidong/p/11764544.html
+ 	@Bean(name = "people")		
 	public People initPeople(@Value("青铜时代号")String name) {
 		People people = new People();
 		people.setName(name);
 		return people;
 	}
-}
+ */
+
+
+
+
+
+
---
+++ src/main/java/com/matrix/entity/Wife.java
@@ -0,0 +1,39 @@
+package com.matrix.entity;
+public class Wife {
+	private String word;
+	public Wife() {} 
+	public Wife(String word) {
+		this.word = word;
+	}
+	public void warning() {
+		System.out.println(word);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class aaa{
+	
+}
